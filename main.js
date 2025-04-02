9f80bbf8d77f12858ceb47d1512534ca4542a3d6
Ekbladh <139871151+Ekbladh@users.noreply.github.com>
2025-04-02 14:36:08 +0200

    load data feature tested and working

---
+++ main.js
@@ -0,0 +1,16 @@
+let users = [];
+let posts = [];
+
+async function loadData() {
+  const userResponse = await fetch("data/users.json");
+  const userJSON = await userResponse.json();
+  users = userJSON.users;
+
+  const postResponse = await fetch("data/posts.json");
+  const postJSON = await postResponse.json();
+  posts = postJSON.posts;
+
+  
+}
+
+loadData();
